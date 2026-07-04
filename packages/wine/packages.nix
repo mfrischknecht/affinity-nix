c1ca19ef5181731979385869b9d2d8f624847f81
Manuel Frischknecht <manuel.frischknecht@gmail.com>
2026-07-04 18:49:15 +0200

    Remove systemtap dependency from glib

--- packages/wine/packages.nix
+++ packages/wine/packages.nix
@@ -4,9 +4,48 @@
   pkgs,
   inputs,
   stdPath,
+  lib,
 }:
 let
-  wineUnstable = inputs.nixpkgs.legacyPackages.${stdenv.hostPlatform.system}.wineWow64Packages.full;
+  fixGlibBuildOverlay =
+    (final: prev: {
+      systemtap-sdt = final.runCommand "systemtap-sdt-stub" { } ''
+        mkdir -p $out/include/sys
+        touch $out/include/sys/sdt.h
+      '';
+
+      glib = prev.glib.overrideAttrs (old: {
+          mesonFlags = (builtins.filter
+              (flag: !(lib.hasPrefix "-Ddtrace" flag) && !(lib.hasPrefix "-Dsystemtap" flag))
+              (old.mesonFlags or [ ])
+            ) ++ [
+              "-Ddtrace=disabled"
+              "-Dsystemtap=disabled"
+            ];
+
+        buildInputs = builtins.filter
+          (pkg: pkg.pname or "" != "libsystemtap")
+          old.buildInputs;
+        
+        nativeBuildInputs = builtins.filter
+          (pkg: pkg.pname or "" != "libsystemtap")
+          old.nativeBuildInputs;
+      });
+    });
+
+  patchedNixpkgs =
+    (import inputs.nixpkgs {
+      system = stdenv.hostPlatform.system;
+      overlays = [
+        fixGlibBuildOverlay
+        (final: prev: {
+          pkgsi686Linux = prev.pkgsi686Linux.extend fixGlibBuildOverlay;
+          # buildPackages  = prev.buildPackages.extend fixGlibBuildOverlay;
+        })
+      ];
+    });
+
+  wineUnstable = patchedNixpkgs.wineWow64Packages.full;
 
   symlink = callPackage ./symlink.nix { };
 
@@ -23,6 +62,6 @@ in
   inherit wineUnwrapped;
 
   wine = wrapWithPrefix wineUnwrapped "wine";
-  winetricks = wrapWithPrefix pkgs.winetricks "winetricks";
+  winetricks = wrapWithPrefix patchedNixpkgs.winetricks "winetricks";
   wineserver = wrapWithPrefix wineUnwrapped "wineserver";
 }
